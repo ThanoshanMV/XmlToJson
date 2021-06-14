981f12468f21dd72f51dd68d738613e6ba9bb5d4
MV Thanoshan <mvthanoshan9@gmail.com>
2021-06-14 08:26:09 +0530

    add xml to json parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+declare var require: any;
+var fastXmlParser = require('fast-xml-parser');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'XmlToJson';
+  jsonObject!: object;
+  selectedFile: any;
+  stringfyObject: any;
+  xmlData: any;
+  fileName: string = "file.xml";
+  xmlText: any;
+
+  selectExcelFile(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.fileName = this.selectedFile.name;
+  }
+
+  excelToJsonConverter(xmlTextString: string) {
+    // check if selectedFile is available
+    if (this.selectedFile) {
+      let fileReader = new FileReader();
+      fileReader.readAsBinaryString(this.selectedFile);
+      fileReader.onload = (event: Event) => {
+        this.xmlData = fileReader.result;
+        this.jsonObject = fastXmlParser.parse(this.xmlData, {
+          ignoreAttributes: false,
+          attributeNamePrefix: "",
+        });
+        console.log("xmlToJson", this.jsonObject);
+        this.stringfyObject = JSON.stringify(this.jsonObject, null, 2);
+        this.postDataToAWSAPIGateway(this.stringfyObject);
+        return;
+      };
+    }
+    // check if text area is available
+    if (xmlTextString) {
+      this.xmlData = xmlTextString;
+      this.jsonObject = fastXmlParser.parse(this.xmlData, {
+        ignoreAttributes: false,
+        attributeNamePrefix: "",
+      });
+      console.log("xmlToJson", this.jsonObject);
+      this.stringfyObject = JSON.stringify(this.jsonObject, null, 2);
+      this.postDataToAWSAPIGateway(this.stringfyObject);
+      return;
+    }
+    return (this.stringfyObject =
+      "Please add the XML text either via file or in textfield!");
+  }
+
+  async copyToClipboard(jsonTextString: string) {
+    await navigator.clipboard.writeText(jsonTextString);
+  }
+
+  postDataToAWSAPIGateway(jsonTextString: string) {
+    const data = { JsonData: jsonTextString };
+
+    fetch("https://0bwkyz0kha.execute-api.ap-south-1.amazonaws.com/default/XmlToJsonAPI", {
+      method: "POST", // or 'PUT'
+      mode: 'no-cors', // no-cors, *cors, same-origin
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {console.log("Success:", data);})
+      .catch((error) => {console.error("Error:", error);});
+  }
 }
